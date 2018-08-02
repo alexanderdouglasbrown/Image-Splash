1a004621e42d073f9af020209386cf17f5d65c1b
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2018-08-01 23:01:23 -0700

    Back to express-session

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
 app.use(methodOverride("_method"))
 
-app.use(require("cookie-session")({
+app.use(require("express-session")({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
     resave: false
