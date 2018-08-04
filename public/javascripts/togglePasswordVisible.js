9b8ba052e09e3c49f4c537e381d5c444139ef6e3
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2018-08-04 16:00:59 -0700

    Visible password toggle on registration page

---
+++ public/javascripts/togglePasswordVisible.js
@@ -0,0 +1,16 @@
+function togglePasswordVisible(){
+    
+
+    const passwordBox = document.getElementById("password")
+    const eyeIcon = document.getElementById("password-toggle-eye")
+    
+    if (passwordBox.type == "password"){
+        passwordBox.type = "input"
+        eyeIcon.classList.remove("fa-eye-slash")
+        eyeIcon.classList.add("fa-eye")
+    } else {
+        passwordBox.type = "password"
+        eyeIcon.classList.remove("fa-eye")
+        eyeIcon.classList.add("fa-eye-slash")
+    }
+}
